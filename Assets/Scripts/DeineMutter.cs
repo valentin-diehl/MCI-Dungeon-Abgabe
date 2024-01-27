7fc32e561b37c5e2d0f84a6978c3a57ef0d00182
Vivien Berg <vivien.berg@student.hs-rm.de>
2024-01-27 15:53:02 +0100

    deine Mutter Commit

---
+++ Assets/Scripts/DeineMutter.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeineMutter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // deine Mutter ist so fett, Google Earth hat an gerufen, sie steht im Weg
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
