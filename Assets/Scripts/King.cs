23e7ad4d2d1cf06f6134c971baa9500b8527242b
valentin-diehl <hello.valentindiehl@gmail.com>
2024-02-12 19:08:36 +0100

    updatet king and pawn

--- Assets/Scripts/King.cs
+++ Assets/Scripts/King.cs
@@ -16,9 +16,39 @@ public class King : ChessPiece {
         if(difY == 1 && difX == 0 || difX == 1 && difY == 0 || difY == 1 && difX == 1){
             hasMoved = true; 
         }
-        
-        
         return false;
+    }
+    
+    private bool IsValidRochade(Vector2 newPos){
+
+        if (hasMoved) return false;
+        if (newPos.y != 0 && (int)newPos.y != 7) return false;
+
+        var difX = Math.Abs(x - newPos.x);
+        var difY = Math.Abs(y - newPos.y);
+        if (difX is 2 && difY is 0){
+            if (x > newPos.x){
+                switch (Player.GetColor()){
+                    case "Weiß" when Pieces[new Vector2(0, 0)].hasMoved:
+                    case "Schwarz" when Pieces[new Vector2(0, 7)].hasMoved:
+                        return false;
+                }
+                for (var i = 1; i < 4; i++)
+                    if (Pieces[new Vector2(i, newPos.y)] != null) return false;
+            }
+            else{
+                switch (Player.GetColor()){
+                    case "Weiß" when Pieces[new Vector2(7, 0)].hasMoved:
+                    case "Schwarz" when Pieces[new Vector2(7, 7)].hasMoved:
+                        return false;
+                }
+                for (var i = 5; i < 7; i++)
+                    if (Pieces[new Vector2(i, newPos.y)] != null) return false;
+
+            }
+        }
+
+        return true;
     }
 
 }
